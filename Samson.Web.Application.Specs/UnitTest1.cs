a3d0d0a6acfed5984899950ddcf2097a78d870b2
gladki24 <seweryngla@hotmail.com>
2021-10-24 15:43:33 +0200

    Add NUnit NuGet

---
+++ Samson.Web.Application.Specs/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Samson.Web.Application.Specs
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
