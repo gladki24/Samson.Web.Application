5575b1f82b4ca6a92fda7a2d38bf9e87be31e46f
gladki24 <seweryngla@hotmail.com>
2021-10-24 13:14:59 +0200

    Add Autofac NuGet and configure DI container

--- Samson.Web.Application/Program.cs
+++ Samson.Web.Application/Program.cs
@@ -1,11 +1,6 @@
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Samson.Web.Application
 {
@@ -18,6 +13,7 @@ namespace Samson.Web.Application
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
