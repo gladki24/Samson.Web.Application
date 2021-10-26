5eea11ddbd69fbb7df32b535c5d64e4de564cd89
gladki24 <seweryngla@hotmail.com>
2021-10-26 21:14:43 +0200

    #9 add convention unit tests

---
+++ Samson.Web.Application.Infrastructure/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Samson.Web.Application.Infrastructure
+{
+    /// <summary>
+    /// Extensions for string type
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Checks string is null or empty
+        /// </summary>
+        /// <param name="s">Standard .NET string</param>
+        /// <returns>Specifies is null or empty string</returns>
+        public static bool IsNullOrEmpty(this string s)
+        {
+            return string.IsNullOrEmpty(s);
+        }
+    }
+}
